d06244e003f30beec885eff7d19e83558d94dc23
Shahzain Ahmed <shahzainahmed57@gmail.com>
2025-01-10 01:33:38 +0500

    Update BottomSheet background color to use AppColors.kTransparentColor

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           scaffoldBackgroundColor: AppColors.kBackgroundColor,
           bottomSheetTheme: const BottomSheetThemeData(
-            backgroundColor: Colors.transparent,
+            backgroundColor: AppColors.kTransparentColor,
           ),
           appBarTheme: const AppBarTheme(
             backgroundColor: AppColors.kBackgroundColor,
